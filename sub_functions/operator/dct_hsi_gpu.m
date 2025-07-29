49851dae2bd303485edd6f5f2112b3c503e54566
Shingo Takemoto <takemoto.s.e908@m.isct.ac.jp>
2025-07-29 17:54:04 +0900

    Remove unnecessary files

--- sub_functions/operator/dct_hsi_gpu.m
+++
@@ -1,5 +0,0 @@
-function[z] = dct_hsi_gpu(z,sizeof)
-
-zr = reshape(z, sizeof(1)*sizeof(2), sizeof(3));
-zr = dct(transpose(zr));
-z = reshape(transpose(zr),sizeof);
--- sub_functions/operator/idct_hsi_gpu.m
+++
@@ -1,5 +0,0 @@
-function[z] = idct_hsi_gpu(z,sizeof)
-
-zr = reshape(z, sizeof(1)*sizeof(2), sizeof(3));
-zr = idct(transpose(zr));
-z = reshape(transpose(zr),sizeof);
