b2993e13759dc36a4fa7658d7ed67649e583783f
Shingo Takemoto <takemoto.s.af@m.titech.ac.jp>
2025-10-03 23:35:03 +0900

    Update PPDS functions

--- methods/S3TTV/func_S3TTV_gst_for_denoising_PPDS.m
+++ methods/S3TTV/func_S3TTV_gst_for_denoising_PPDS.m
@@ -13,8 +13,8 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function [HSI_restored, removed_noise, other_result] ...
-     = func_S3TTV_gst_for_denoising_by_PsPPDS(HSI_clean, HSI_noisy, params)
-fprintf('** Running func_S3TTV_gst_for_denoising_by_PsPPDS **\n');
+     = func_S3TTV_gst_for_denoising_PPDS(HSI_clean, HSI_noisy, params)
+fprintf('** Running func_S3TTV_gst_for_denoising_PPDS **\n');
 HSI_clean = single(HSI_clean);
 HSI_noisy  = single(HSI_noisy);
 HSI_noisy_gpu = gpuArray(single(HSI_noisy));
@@ -23,16 +23,16 @@ HSI_noisy_gpu = gpuArray(single(HSI_noisy));
 epsilon     = gpuArray(single(params.epsilon));
 alpha       = gpuArray(single(params.alpha));
 beta        = gpuArray(single(params.beta));
-boundary    = params.boundary;
+% boundary    = params.boundary;
 blocksize   = gpuArray(single(params.blocksize));
-shiftstep   = gpuArray(single(params.shiftstep));
+% shiftstep   = gpuArray(single(params.shiftstep));
 maxiter     = gpuArray(single(params.maxiter));
 stopcri     = gpuArray(single(params.stopcri));
-tmp_save_name   = params.tmp_save_name;
+% tmp_save_name   = params.tmp_save_name;
 
 %% Setting params
 disprate    = gpuArray(single(100));
-saverate    = gpuArray(single(1000));
+% saverate    = gpuArray(single(1000));
 
 
 %% Initializing primal and dual variables
@@ -59,69 +59,85 @@ T = zeros([n1, n2, n3], 'single', 'gpuArray');
 % Y3 = Dv(T)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-Y1 = zeros([n1, n2, n3, 2, blocksize(1)/shiftstep(1), blocksize(2)/shiftstep(2)], 'single', 'gpuArray');
+% Y1 = zeros([n1, n2, n3, 2, blocksize(1)/shiftstep(1), blocksize(2)/shiftstep(2)], 'single', 'gpuArray');
+Y1 = zeros([n1, n2, n3, 2, blocksize(1), blocksize(2)], 'single', 'gpuArray');
 Y2 = zeros([n1, n2, n3], 'single', 'gpuArray');
 Y3 = zeros([n1, n2, n3], 'single', 'gpuArray');
 
 %% Setting operator and stepsize for Pock's P-PDS
 
-switch boundary
-    case 'Neumann' % Difference operators with Neumann boundary
-        D       = @(z) cat(4, z([2:end, end],:,:) - z, z(:,[2:end, end],:) - z);
-        Dt      = @(z) cat(1, -z(1, :, :, 1), -z(2:end-1, :, :, 1) + z(1:end-2, :, :, 1), z(end-1, :, :, 1)) ...
-                + cat(2, -z(:, 1, :, 2), -z(:, 2:end-1, :, 2) + z(:, 1:end-2, :, 2), z(:, end-1, :, 2));
-        Dv      = @(z) z([2:end, end],:,:) - z;
-        Dvt     = @(z) cat(1, -z(1, :, :), -z(2:(n1-1), :, :) + z(1:(n1-2), :, :), z(n1-1, :, :));
-        Ds      = @(z) z(:, :, [2:end, end], :) - z;
-        Dst     = @(z) cat(3, -z(:, :, 1), -z(:, :, 2:n3-1) + z(:, :, 1:n3-2), z(:, :, n3-1));
-        
-        one = ones(n1, n2, n3);
+% switch boundary
+%     case 'Neumann' % Difference operators with Neumann boundary
+%         D       = @(z) cat(4, z([2:end, end],:,:) - z, z(:,[2:end, end],:) - z);
+%         Dt      = @(z) cat(1, -z(1, :, :, 1), -z(2:end-1, :, :, 1) + z(1:end-2, :, :, 1), z(end-1, :, :, 1)) ...
+%                 + cat(2, -z(:, 1, :, 2), -z(:, 2:end-1, :, 2) + z(:, 1:end-2, :, 2), z(:, end-1, :, 2));
+%         Dv      = @(z) z([2:end, end],:,:) - z;
+%         Dvt     = @(z) cat(1, -z(1, :, :), -z(2:(n1-1), :, :) + z(1:(n1-2), :, :), z(n1-1, :, :));
+%         Ds      = @(z) z(:, :, [2:end, end], :) - z;
+%         Dst     = @(z) cat(3, -z(:, :, 1), -z(:, :, 2:n3-1) + z(:, :, 1:n3-2), z(:, :, n3-1));
+% 
+%         one = ones(n1, n2, n3);
+% 
+%         precon1_Dv = cat(1, one(1, :, :), 2*one(2:(n1-1), :, :), one(n1-1, :, :));
+%         precon1_Dh = cat(2, one(:, 1, :), 2*one(:, 2:(n2-1), :), one(:, n2-1, :));
+%         precon1_Db = cat(3, one(:, :, 1), 2*one(:, :, 2:(n3-1)), one(:, :, n3-1));
+% 
+%         zero_para = 100;
+% 
+%         precon2_Dv = cat(1, 2*ones(n1-1, n2), zero_para*ones(1, n2)).*ones(n1, n2, n3);
+%         precon2_Dh = cat(2, 2*ones(n1, n2-1), zero_para*ones(n1, 1)).*ones(n1, n2, n3);
+%         precon2_Db = cat(3, 2*ones(n1, n2, n3-1), zero_para*ones(n1, n2, 1));
+% 
+%         gamma1_U    = gpuArray(single(1./(...
+%             (precon1_Dv.*precon1_Db + precon1_Dh.*precon1_Db)*prod(blocksize)/prod(shiftstep) + 1)));
+%         gamma1_S    = gpuArray(single(1));
+%         gamma1_T    = gpuArray(single(1./(precon1_Dv + 1)));
+%         gamma2_Y1_1 = gpuArray(single(1/(precon2_Dv.*precon2_Db)));
+%         gamma2_Y1_2 = gpuArray(single(1/(precon2_Dh.*precon2_Db)));
+%         gamma2_Y1 = cat(4, gamma2_Y1_1, gamma2_Y1_2);
+%         gamma2_Y2   = gpuArray(single(1/3));
+%         gamma2_Y3   = gpuArray(single(1./(precon2_Dv)));
+% 
+%         %memory release
+%         clear precon1_Dv precon1_Dh precon1_Db
+%         clear precon2_Dv precon1_Dh precon1_Db
+%         clear zero_para one
+%         clear gamma2_Y1_1 gamma2_Y1_2
+% 
+%     case 'circulant' % Difference operators with circulant boundary
+%         D       = @(z) cat(4, z([2:end, 1],:,:) - z, z(:,[2:end, 1],:) - z);
+%         Dt      = @(z) z([end,1:end-1],:,:,1) - z(:,:,:,1) + z(:,[end,1:end-1],:,2) - z(:,:,:,2);
+%         Dv      = @(z) z([2:end, 1],:,:) - z;
+%         Dvt     = @(z) z([end,1:end-1],:,:) - z(:,:,:);
+%         Ds      = @(z) z(:, :, [2:end, 1], :) - z;
+%         Dst     = @(z) z(:,:,[end,1:end-1],:) - z(:,:,:,:);
+% 
+%         gamma1_U    = gpuArray(single(1./(prod(blocksize)/prod(shiftstep) * 2*2 * 2 + 1))); % P*D*Ds + I
+%         gamma1_S    = gpuArray(single(1));
+%         gamma1_T    = gpuArray(single(1/(2 + 1)));
+%         gamma2_Y1   = gpuArray(single(1/(2*2)));
+%         gamma2_Y2   = gpuArray(single(1/3));
+%         gamma2_Y3   = gpuArray(single(1/2));
+% 
+% end
 
-        precon1_Dv = cat(1, one(1, :, :), 2*one(2:(n1-1), :, :), one(n1-1, :, :));
-        precon1_Dh = cat(2, one(:, 1, :), 2*one(:, 2:(n2-1), :), one(:, n2-1, :));
-        precon1_Db = cat(3, one(:, :, 1), 2*one(:, :, 2:(n3-1)), one(:, :, n3-1));
-        
-        zero_para = 100;
-        
-        precon2_Dv = cat(1, 2*ones(n1-1, n2), zero_para*ones(1, n2)).*ones(n1, n2, n3);
-        precon2_Dh = cat(2, 2*ones(n1, n2-1), zero_para*ones(n1, 1)).*ones(n1, n2, n3);
-        precon2_Db = cat(3, 2*ones(n1, n2, n3-1), zero_para*ones(n1, n2, 1));
+D       = @(z) cat(4, z([2:end, 1],:,:) - z, z(:,[2:end, 1],:) - z);
+Dt      = @(z) z([end,1:end-1],:,:,1) - z(:,:,:,1) + z(:,[end,1:end-1],:,2) - z(:,:,:,2);
+Dv      = @(z) z([2:end, 1],:,:) - z;
+Dvt     = @(z) z([end,1:end-1],:,:) - z(:,:,:);
+Ds      = @(z) z(:, :, [2:end, 1], :) - z;
+Dst     = @(z) z(:,:,[end,1:end-1],:) - z(:,:,:,:);
 
-        gamma1_U    = gpuArray(single(1./(...
-            (precon1_Dv.*precon1_Db + precon1_Dh.*precon1_Db)*prod(blocksize)/prod(shiftstep) + 1)));
-        gamma1_S    = gpuArray(single(1));
-        gamma1_T    = gpuArray(single(1./(precon1_Dv + 1)));
-        gamma2_Y1_1 = gpuArray(single(1/(precon2_Dv.*precon2_Db)));
-        gamma2_Y1_2 = gpuArray(single(1/(precon2_Dh.*precon2_Db)));
-        gamma2_Y1 = cat(4, gamma2_Y1_1, gamma2_Y1_2);
-        gamma2_Y2   = gpuArray(single(1/3));
-        gamma2_Y3   = gpuArray(single(1./(precon2_Dv)));
+gamma1_U    = gpuArray(single(1./(prod(blocksize) * 2*2 * 2 + 1))); % P*D*Ds + I
+gamma1_S    = gpuArray(single(1));
+gamma1_T    = gpuArray(single(1/(2 + 1)));
+gamma2_Y1   = gpuArray(single(1/(2*2)));
+gamma2_Y2   = gpuArray(single(1/3));
+gamma2_Y3   = gpuArray(single(1/2));
 
-        %memory release
-        clear precon1_Dv precon1_Dh precon1_Db
-        clear precon2_Dv precon1_Dh precon1_Db
-        clear zero_para one
-        clear gamma2_Y1_1 gamma2_Y1_2
-
-    case 'circulant' % Difference operators with circulant boundary
-        D       = @(z) cat(4, z([2:end, 1],:,:) - z, z(:,[2:end, 1],:) - z);
-        Dt      = @(z) z([end,1:end-1],:,:,1) - z(:,:,:,1) + z(:,[end,1:end-1],:,2) - z(:,:,:,2);
-        Dv      = @(z) z([2:end, 1],:,:) - z;
-        Dvt     = @(z) z([end,1:end-1],:,:) - z(:,:,:);
-        Ds      = @(z) z(:, :, [2:end, 1], :) - z;
-        Dst     = @(z) z(:,:,[end,1:end-1],:) - z(:,:,:,:);
-
-        gamma1_U    = gpuArray(single(1./(prod(blocksize)/prod(shiftstep) * 2*2 * 2 + 1))); % P*D*Ds + I
-        gamma1_S    = gpuArray(single(1));
-        gamma1_T    = gpuArray(single(1/(2 + 1)));
-        gamma2_Y1   = gpuArray(single(1/(2*2)));
-        gamma2_Y2   = gpuArray(single(1/3));
-        gamma2_Y3   = gpuArray(single(1/2));
-
-end
 
 % Expansion operators
-P = @(z) func_PeriodicExpansion(z, blocksize, shiftstep);
+P = @(z) func_PeriodicExpansion(z, blocksize);
 Pt = @(z) func_PeriodicExpansionTrans(z);
 
 
@@ -146,35 +162,38 @@ for i = 1:maxiter
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     U_tmp   = U - gamma1_U.*(Dst(Dt(Pt(Y1))) + Y2);
     U_next  = ProjBox(U_tmp, 0, 1);
+    U_res   = 2*U_next - U;
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % Updating S
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     S_tmp   = S - gamma1_S.*Y2;
     S_next  = ProjFastL1Ball(S_tmp, alpha);
+    S_res   = 2*S_next - S;
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % Updating T
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     T_tmp   = T - gamma1_T.*(Y2 + Dvt(Y3));
     T_next  = ProjFastL1Ball(T_tmp, beta);
+    T_res   = 2*T_next - T;
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % Updating Y1
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    Y1_tmp  = Y1 + gamma2_Y1.*(P(D(Ds(2*U_next - U))));
+    Y1_tmp  = Y1 + gamma2_Y1.*(P(D(Ds(U_res))));
     Y1_next = Y1_tmp - gamma2_Y1.*Prox_S3TTV(Y1_tmp./gamma2_Y1, 1./gamma2_Y1, blocksize);
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % Updating Y2
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    Y2_tmp  = Y2 + gamma2_Y2.*(2*(U_next + S_next + T_next) - (U + S + T));    
+    Y2_tmp  = Y2 + gamma2_Y2.*(U_res + S_res + T_res);    
     Y2_next = Y2_tmp - gamma2_Y2.*ProjL2ball(Y2_tmp./gamma2_Y2, HSI_noisy_gpu, epsilon);
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % Updating Y3
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    Y3_next = Y3 + gamma2_Y3.*Dv(2*T_next - T);
+    Y3_next = Y3 + gamma2_Y3.*Dv(T_res);
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % Calculating error
@@ -228,21 +247,21 @@ for i = 1:maxiter
             title('l2ball')
             drawnow
     end
-    if (mod(i, saverate) == 0)
-        U_curr = gather(U);
-        S_curr = gather(S);
-        T_curr = gather(T);
-        Y1_curr = gather(Y1);
-        Y2_curr = gather(Y2);
-        Y3_curr = gather(Y3);
-        i_curr = gather(i);
-
-        save(tmp_save_name, ...
-            'U_curr', 'S_curr', 'T_curr', 'Y1_curr', 'Y2_curr', 'Y3_curr', 'i_curr', ...
-            'converge_rate_U', 'converge_rate_S', 'converge_rate_T', 'converge_rate_N', ...
-            'running_time', 'move_mpsnr', 'move_mssim', 'l2ball', 'params', ...
-            '-v7.3', '-nocompression');
-    end
+    % if (mod(i, saverate) == 0)
+    %     U_curr = gather(U);
+    %     S_curr = gather(S);
+    %     T_curr = gather(T);
+    %     Y1_curr = gather(Y1);
+    %     Y2_curr = gather(Y2);
+    %     Y3_curr = gather(Y3);
+    %     i_curr = gather(i);
+    % 
+    %     save(tmp_save_name, ...
+    %         'U_curr', 'S_curr', 'T_curr', 'Y1_curr', 'Y2_curr', 'Y3_curr', 'i_curr', ...
+    %         'converge_rate_U', 'converge_rate_S', 'converge_rate_T', 'converge_rate_N', ...
+    %         'running_time', 'move_mpsnr', 'move_mssim', 'l2ball', 'params', ...
+    %         '-v7.3', '-nocompression');
+    % end
 end
 
 fprintf('Iter: %d, Error: %0.6f, MPSNR: %#.4g, MSSIM: %#.4g, Time: %0.2f.\n', ...
